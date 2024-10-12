625cbd5dd0aed39d44901a6fe69ff76ff8eb8d96
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-10-12 00:45:38 -0400

    added coin calculation with stopwatch

--- steersafe/ViewModels/HomePageModel.swift
+++ steersafe/ViewModels/HomePageModel.swift
@@ -2,18 +2,20 @@ import Foundation
 import SwiftUI
 
 class HomePageModel: ObservableObject {
-    // You can have state variables here that affect your view
     @Published var isDriving: Bool = false
+    @Published var time: TimeInterval = 0  // Total time in seconds
+    @Published var coins: Int = 0           // Total coins earned
 
     // Function to start driving mode
     func startDriving() {
         isDriving = true
+        time = 0
         // Additional logic for when driving mode starts
     }
 
     // Function to stop driving mode
     func stopDriving() {
         isDriving = false
-        // Additional logic for when driving mode stops
+        coins = Int(time) / 120  // Calculate 1 coin per 2 minutes of driving, each coin worth 10 cents
     }
 }
